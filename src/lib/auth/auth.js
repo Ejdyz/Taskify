b278bbd134aa617f985d734d107768cbaa44cd38
Ejdy <adamhonzikcz@seznam.cz>
2025-02-13 20:43:33 +0100

    fix: added trustHost on nextAuth handler

--- src/lib/auth/auth.js
+++ src/lib/auth/auth.js
@@ -7,4 +7,5 @@ import GitHub from "next-auth/providers/github"
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [ Google, GitHub ],
+    trustHost: true,
 });
