ee3469711bb4f89b18e87cd5f40da2b433e1c17c
Párek8 <polakpetr8878@gmail.com>
2025-02-13 09:36:24 +0100

    feat: login using Google OAuth && setup of Prisma

--- src/components/todoList/TodoListCard.jsx
+++ src/components/todoList/TodoListCard.jsx
@@ -1,14 +1,15 @@
+"use client"
 import {Card, CardHeader, CardBody, CardFooter} from "@heroui/card";
+import { signIn } from "next-auth/react"
+import {Button} from "@heroui/button"
 const TodoListCard = () => {
   return (
     <Card className="max-w-[400px]">
       <CardHeader className="flex gap-3">
-        <div className="flex flex-col">
-          <p className="text-md">HeroUI</p>
-          <p className="text-small text-default-500">heroui.com</p>
-        </div>
+      <button onClick={() => signIn("google")}></button>  
       </CardHeader>
       <CardBody>
+      <Button onClick={() => signIn("google")}></Button>  
         <p>Make beautiful websites regardless of your design experience.</p>
       </CardBody>
       <CardFooter>
