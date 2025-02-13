eedf724213d17e967a85b7d6206bad928ffcd9a8
Ejdy <adamhonzikcz@seznam.cz>
2025-02-13 20:42:01 +0100

    feat: added function to get provider icon

--- src/components/lib/utils.js
+++ src/components/lib/utils.js
@@ -1,6 +1,18 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { GoogleIcon, GitHubIcon } from "../icons/Icons"
  
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
+}
+
+export function getProviderIcon(providerId, props) {
+  switch (providerId) {
+    case "google":
+      return <GoogleIcon {...props}/>
+    case "github":
+      return <GitHubIcon {...props}/>
+    default:
+      break;
+  }
 }
