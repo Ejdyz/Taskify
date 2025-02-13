724caa8d10361154c508e1968becf551d73f676c
Ejdy <adamhonzikcz@seznam.cz>
2025-02-13 20:51:59 +0100

    feat: implemented signout

--- src/components/navigation/AccountMenu.jsx
+++ src/components/navigation/AccountMenu.jsx
@@ -1,24 +1,29 @@
 "use client"
-import { Button } from "@heroui/button"
-import { NavbarContent, NavbarItem } from "@heroui/navbar"
-import ThemeChanger from "../lib/ThemeSwitcher"
+
+// Utils
 import { signOut } from "next-auth/react"
+// Components
+import ThemeChanger from "../lib/ThemeSwitcher"
 import { Avatar } from "@heroui/avatar"
 import { Popover, PopoverTrigger, PopoverContent } from "@heroui/popover"
 import { User } from "@heroui/user"
 import { Divider } from "@heroui/divider"
+import { Button } from "@heroui/button"
+import { NavbarContent, NavbarItem } from "@heroui/navbar"
+// Icons
+import { LogoutIcon } from "../icons/Icons"
 
 const AccountMenu = ({ user }) => {
   return (
     <>
       <NavbarContent justify="end">
-        <Popover placement="bottom-start">
+        <Popover placement="bottom-center">
           <NavbarItem>
             <PopoverTrigger>
-              <Avatar size="sm" isBordered src={user.image}/>
+              <Avatar size="sm" isBordered src={user.image} />
             </PopoverTrigger>
           </NavbarItem>
-          <PopoverContent className="p-4 flex flex-col w-full gap-2" placement="bottom-center">
+          <PopoverContent className="p-4 flex flex-col w-full gap-4" placement="bottom-center">
             <User
               as="button"
               avatarProps={{
@@ -26,13 +31,13 @@ const AccountMenu = ({ user }) => {
                 src: user.image,
                 size: "sm",
               }}
-              className="transition-transform"
+              className="cursor-default"
               description={user.email}
               name={user.name}
             />
-            <Divider/>
-            <ThemeChanger  />
-            <Button fullWidth size="sm">
+            <Divider />
+            <ThemeChanger />
+            <Button onPress={signOut} fullWidth size="sm" color="danger" variant="faded" endContent={<LogoutIcon className={"size-4 rotate-180"} />}>
               Log out
             </Button>
           </PopoverContent>
