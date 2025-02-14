31006440652c6989a644f3a4439aeb7a1f59d7df
Jan Adam <adamhonzikcz@seznam.cz>
2025-02-14 19:42:42 +0100

    feat: added dark and light mode border color

--- src/components/mainPage/FilterNavbar.jsx
+++ src/components/mainPage/FilterNavbar.jsx
@@ -8,7 +8,7 @@ import { GridIcon, ListIcon, HeartIcon, HeartSlashIcon, UnlimitedIcon, MenuBoard
 const FilterNavbar = () => {
   return ( 
     <div className="w-full z-20 md:px-0 px-2">
-      <div className="border-2  flex md:w-3/4 w-full mx-auto justify-between items-center p-2 rounded-2xl">
+      <div className="border-2 border-border flex md:w-3/4 w-full mx-auto justify-between items-center p-2 rounded-2xl">
         <Button color="primary" variant="shadow" className="font-bold" endContent={<MenuBoardIcon />} >Create</Button>
         <div className="flex gap-4">
           <Tabs>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,27 +1,46 @@
-const {heroui} = require("@heroui/theme");
+const { heroui } = require("@heroui/theme");
 
 /** @type {import('tailwindcss').Config} */
-const tailwindConfig =  {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-  	extend: {
-  		colors: {
-  			background: 'rgba(var(--background),1)',
-  			foreground: 'hsl(var(--foreground))',
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
-  },
-  darkMode: ["class"],
-  plugins: [heroui(), require("tailwindcss-animate")],
+const tailwindConfig = {
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: 'rgba(var(--background),1)',
+				foreground: 'hsl(var(--foreground))',
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			}
+		}
+	},
+	darkMode: ["class"],
+	plugins: [
+		heroui({
+			themes: {
+				light: {
+					colors: {
+						border: {
+							DEFAULT: "#E5E7EB",
+						},
+					}
+				},
+				dark: {
+					colors: {
+						border: {
+							DEFAULT: "#47474a",
+						}
+					}
+				}
+			}
+		}),
+		require("tailwindcss-animate")],
 };
 export default tailwindConfig
