9a69b82a03695c5594269be48b80cd92b1c3358e
Párek8 <petrpolak11@zak.spsul.cz>
2025-02-18 13:22:43 +0100

    feat: creation of subtasks

--- src/lib/task.ts
+++ src/lib/task.ts
@@ -2,7 +2,7 @@ import { SubTask, Task } from "@prisma/client";
 import prisma from "./prisma/prisma";
 import { getUserIdFromSessionToken } from "./user/user";
 
-export const createTask = async (taskTitle: string, subTasks: SubTask[]) => {
+export const createTask = async (taskTitle: string, subTasks: any[]) => {
 
     const authorId = await getUserIdFromSessionToken();
 
@@ -23,11 +23,13 @@ export const createTask = async (taskTitle: string, subTasks: SubTask[]) => {
     return task;
 }
 
-export const createSubTasks = async (parentTaskId: string, subTasks: SubTask[]) => {
+export const createSubTasks = async (parentTaskId: string, subTasks: any[]) => {
+    console.log(subTasks);
+    
     return subTasks.forEach(async (t) => {
-        const subtask = await prisma.subTask.createManyAndReturn({
+        const subtask = await prisma.subTask.create({
             data: {
-                content: t.content,
+                content: t.value,
                 isMarked: t.isMarked,
                 taskId: parentTaskId
             }
