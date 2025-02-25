9539f7574bd9b149068b8092d6866450fc5bc0d2
Ejdy <adamhonzikcz@seznam.cz>
2025-02-25 20:30:27 +0100

    feat: added toast provider

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,11 +1,12 @@
 
 import { HeroUIProvider } from "@heroui/system"
 import {ThemeProvider} from "next-themes";
-
+import { ToastProvider } from "@heroui/toast";
 const Providers = ({children}) => {
   return ( 
     <HeroUIProvider >
       <ThemeProvider attribute="data-theme" defaultTheme="system">
+        <ToastProvider /> 
         {children}
       </ThemeProvider>
     </HeroUIProvider>
