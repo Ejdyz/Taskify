ee4331e897edaff3f3e09af2a9a8b0efd5c4bc33
Ejdy <adamhonzikcz@seznam.cz>
2025-02-25 20:41:25 +0100

    feat: implemented function for retrieving task info

--- src/app/(pages)/task/[taskId]/page.jsx
+++ src/app/(pages)/task/[taskId]/page.jsx
@@ -2,7 +2,7 @@
 import { notFound } from 'next/navigation'
 // Components
 import TaskWrapper from '@/components/pages/task-page/TaskWrapper';
-import { image } from '@heroui/theme';
+import { getTaskInfo } from "@/lib/task/task"
 export default async function page(props) {
   const params = await props.params;
   const taskId = params.taskId;
@@ -13,32 +13,7 @@ export default async function page(props) {
   }
 
   // Fetch task data
-  const task = {
-    id: "A1B2C3D4E5F6G7H8I9J0",
-    isFavorite: false,
-    updatedAt: "2023-05-15 14:30:00",
-    title: "Project Alpha",
-    tasks: [
-      { value: "Define scope", isMarked: true },
-      { value: "Research market", isMarked: false },
-      { value: "Create prototype", isMarked: false },
-      { value: "Gather feedback", isMarked: true }
-    ],
-    author: {
-      name: "Alice Johnson",
-      email: "alice@example.com",
-      image: "https://i.pravatar.cc/150?u=a04258114e29026302d"
-    },
-    contributors: [
-      {name: "Bob Smith", email: "bob@example.com", image: "https://i.pravatar.cc/150?u=a04258114e29026302d" },
-      {name: "Charlie Brown", email: "charlie@example.com", image: "https://i.pravatar.cc/150?u=a04258114e29026302d" },
-      {name: "David Green", email: "david@example.com", image: "https://i.pravatar.cc/150?u=a04258114e29026302d" },
-      {name: "Eve White", email: "eve@example.com", image: "https://i.pravatar.cc/150?u=a04258114e29026302d" },
-      {name: "Frank Black", email: "frank@example.com", image: "https://i.pravatar.cc/150?u=a04258114e29026302d" },
-      { name: "Grace Brown", email: "grace@exmaple.com", image: "https://i.pravatar.cc/150?u=a04258114e29026302d" },
-      { name: "Harry Green", email: "harry@example.com", image: "https://i.pravatar.cc/150?u=a04258114e29026302d" },
-    ]
-  }
+  const task = await getTaskInfo(taskId);
 
   if (!task) {
     notFound();
