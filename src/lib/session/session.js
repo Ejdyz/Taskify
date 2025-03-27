fcdf5911ccbd9319e4dad0bef0274da0982db4e3
Ejdy <adamhonzikcz@seznam.cz>
2025-03-27 09:54:43 +0100

    fix: added check for right cookie name

--- src/lib/session/session.js
+++ src/lib/session/session.js
@@ -4,9 +4,13 @@ import { cookies } from "next/headers";
 export const getSession = async () => {
     
     const cookieStore = await cookies();
-    const cookieToken = cookieStore.get("authjs.session-token")?.value ?? null;
+    let cookieToken = null;
+    if (process.env.NODE_ENV === "production") {
+        cookieToken = cookieStore.get("__Secure-authjs.session-token")?.value ?? null;
+    }else{
+        cookieToken = cookieStore.get("authjs.session-token")?.value ?? null;
+    }
 
     return { session: cookieToken };
-
 }
 
